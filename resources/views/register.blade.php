0d33665a8fb5faf5b85b3b8ded6e91c60257ab5d
Keerthan N S <62982853+keerthan2002@users.noreply.github.com>
2022-10-10 08:39:33 -0700

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,29 +1,6 @@
 @extends('master')
 
 @section('content')
-{{-- <div class="container custom-login"  style="width: 30%;background-color:#ccccff;margin-bottom:100px;">
-    <h3 align='center'>Register Now</h3>
-    <div class="row">
-        <div class=""  style="width:100%;padding:10%;">
-            <form action="/register" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="inputforusername">Username</label>
-                    <input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="Enter a username">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter your email ID">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Enter password">
-                </div>
-                <button type="submit" class="btn btn-default">Register</button>
-            </form>
-        </div>
-    </div>
-</div> --}}
     @if(session()->has('failed'))
         <div class="error_container" id="faillogin">
             <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" class="bi bi-exclamation-circle error_icon" viewBox="0 0 16 16">
